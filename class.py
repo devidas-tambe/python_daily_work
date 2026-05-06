91a349b0e7761eb5a3346b3e64b57bce0466494c
devidas-tambe <devitambe102@gmail.com>
2026-05-06 20:24:22 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,14 @@
+class student:
+    name="Devidas tambe"
+
+s1=student()
+print(s1.name)
+
+s2=student()
+print(s2.name)
+
+class car:
+    color="red"
+    model="BMW"
+c1=car()
+print(c1.color)
---
+++ constructor.py
@@ -0,0 +1,9 @@
+class student:
+    def __init__(self, fulname, age):
+        self.name=fulname
+        self.age=age
+
+s1=student("devidas",25)
+print("Name:", s1.name)
+print("Age:", s1.age)
+        
